5f5decae315ad6abe6f55680957e229522e5089d
Martin K. <martin_kuhl@gmx.net>
2025-10-17 22:00:45 +0200

    Logger fix

--- Logger/Handler.php
+++ Logger/Handler.php
@@ -4,16 +4,32 @@ namespace Martinkuhl\AutheliaOidc\Logger;
 
 use Magento\Framework\Logger\Handler\Base;
 use Monolog\Logger;
+use Magento\Framework\App\Filesystem\DirectoryList;
 
 class Handler extends Base
 {
     /**
      * @var string
      */
-    protected $fileName = BP . '/var/log/authelia_oidc.log';
+    protected $fileName = '/var/log/authelia_oidc.log';
 
+    
     /**
      * @var int
      */
-    protected $loggerType = Logger::INFO; // Alle Nachrichten mit Level INFO und höher werden protokolliert
+    protected $loggerType = Logger::DEBUG; // Alle Nachrichten mit Level DEBUG und höher werden protokolliert
+    
+    /**
+     * @param \Magento\Framework\Filesystem\Driver\File $filesystem
+     * @param DirectoryList $directoryList
+     * @param string $filePath
+     */
+    public function __construct(
+        \Magento\Framework\Filesystem\Driver\File $filesystem,
+        DirectoryList $directoryList,
+        $filePath = null
+    ) {
+        $this->fileName = $directoryList->getPath(DirectoryList::VAR_DIR) . '/log/authelia_oidc.log';
+        parent::__construct($filesystem, $filePath);
+    }
 }
