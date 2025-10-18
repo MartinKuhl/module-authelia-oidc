79e352267238dc6db56371537d1931a3af11e472
Martin K. <martin_kuhl@gmx.net>
2025-10-18 19:36:19 +0200

    fix Logging

--- Logger/Handler.php
+++ Logger/Handler.php
@@ -4,32 +4,37 @@ namespace Martinkuhl\AutheliaOidc\Logger;
 
 use Magento\Framework\Logger\Handler\Base;
 use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 use Magento\Framework\App\Filesystem\DirectoryList;
 
-class Handler extends Base
+class Handler extends StreamHandler
 {
     /**
      * @var string
      */
     protected $fileName = '/var/log/authelia_oidc.log';
 
-    
     /**
-     * @var int
-     */
-    protected $loggerType = Logger::DEBUG; // Alle Nachrichten mit Level DEBUG und höher werden protokolliert
-    
-    /**
-     * @param \Magento\Framework\Filesystem\Driver\File $filesystem
      * @param DirectoryList $directoryList
      * @param string $filePath
      */
     public function __construct(
-        \Magento\Framework\Filesystem\Driver\File $filesystem,
         DirectoryList $directoryList,
         $filePath = null
     ) {
-        $this->fileName = $directoryList->getPath(DirectoryList::VAR_DIR) . '/log/authelia_oidc.log';
-        parent::__construct($filesystem, $filePath);
+        // Absoluten Pfad zur Log-Datei festlegen
+        $logfile = $directoryList->getPath(DirectoryList::VAR_DIR) . '/log/authelia_oidc.log';
+        
+        // Stelle sicher, dass die Datei existiert und beschreibbar ist
+        if (!file_exists($logfile)) {
+            touch($logfile);
+            chmod($logfile, 0666);
+        }
+        
+        // StreamHandler mit dem Level DEBUG und der Datei initialisieren
+        parent::__construct($logfile, Logger::DEBUG);
     }
 }
+    
+
+
